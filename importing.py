0116ceddaeb72d46a992c3c8d8c2013049de7f5c
AdityaDavalagar <aditya14315@gmail.com>
2019-03-01 12:32:46 +0530

    Corrected code

--- importing.py
+++ importing.py
@@ -1,27 +1,3 @@
-# import xlrd 
-  
-# loc = ("C:/Users/ramky/Desktop/webscraping/excelsht.xlsx") 
-  
-# wb = xlrd.open_workbook(loc) 
-# sheet = wb.sheet_by_index(0) 
-  
-# # For row 0 and column 0 
-# sheet.cell_value(0, 0) 
-  
-# for i in range(sheet.ncols): 
-#     print(sheet.cell_value(1, i)) 
-
-# import xlrd 
-  
-# loc = ("C:/Users/ramky/Desktop/webscraping/excelsht.xlsx") 
-  
-# wb = xlrd.open_workbook(loc) 
-# sheet = wb.sheet_by_index(0) 
-  
-# sheet.cell_value(0, 0) 
-  
-# print(sheet.row_values(1)) 
-
 from xlrd import open_workbook
 wb = open_workbook('excelsht.xlsx')
 values = []
