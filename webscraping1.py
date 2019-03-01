315a725aaa2fda3b2337cd4c7d247f18f423014d
AdityaDavalagar <aditya14315@gmail.com>
2019-03-01 12:11:12 +0530

    python program to fetch text data from other webpage using web scraping

---
+++ webscraping1.py
@@ -0,0 +1,11 @@
+import requests
+import bs4
+res = requests.get('https://en.wikipedia.org/wiki/Main_Page')
+type(res)
+res.text
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+type(soup)
+req = soup.select('title')
+req
+req[0]
+print(req[0].getText())
